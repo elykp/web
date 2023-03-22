5c277261aebd0ee8f318b609c436d412391359b3
blackparadise0407 <blackparadise0407@gmail.com>
2023-03-22 17:17:50 +0700

    feat: implement file reader async

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,3 +6,14 @@ export const getCDNLink = (url?: string) => {
 	}
 	return `https://res.cloudinary.com/${config.cloudinary.cloudName}/image/fetch/` + url;
 };
+
+export const readFileAsync = (file: File): Promise<string> =>
+	new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
+			resolve(reader.result as string);
+		};
+
+		reader.onerror = reject;
+	});
