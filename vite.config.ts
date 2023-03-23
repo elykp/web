7cdbb696b324102c96d6918258df51a8f1f8f151
blackparadise0407 <blackparadise0407@gmail.com>
2023-03-23 22:50:31 +0700

    config: dynamic alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,26 @@ import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+interface Alias {
+	alias: string;
+	path: string;
+}
+
+// Add new alias here
+const ALIASES: Alias[] = [
+	{ alias: '$lib', path: 'src/lib' },
+	{ alias: '$utils', path: 'src/utils' },
+	{ alias: '$constants', path: 'src/constants' },
+	{ alias: '$components', path: 'src/components' }
+];
+
+const generateAliases = () => {
+	return ALIASES.reduce((acc, cur) => {
+		acc[cur.alias] = path.resolve(__dirname, cur.path);
+		return acc;
+	}, {});
+};
+
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
@@ -13,10 +33,6 @@ export default defineConfig({
 		open: true
 	},
 	resolve: {
-		alias: {
-			$lib: path.resolve(__dirname, 'src/lib'),
-			$utils: path.resolve(__dirname, 'src/utils'),
-			$constants: path.resolve(__dirname, 'src/constants')
-		}
+		alias: generateAliases()
 	}
 });
